d75c45aded09dd87dbf9e83f2f9589e661acb688
vandniBirla <78420545+vandniBirla@users.noreply.github.com>
2021-04-24 18:31:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@ var catImg1,mouseImg1,catImg2,mouseImg2,catImg3,mouseImg3;
 
 function preload() {
     //load the images here
-    bgImg=loadImage("images/garden.png");
-    mouseImg1=loadAnimation("images/mouse1.png");
-    catImg1=loadAnimation("images/cat1.png");
-    mouseImg2=loadAnimation("images/mouse2.png","images/mouse3.png");
-    catImg2=loadAnimation("images/cat2.png","images/cat3.png")
-    mouseImg3=loadAnimation("images/mouse4.png");
-    catImg3=loadAnimation("images/cat4.png");
+    bgImg=loadImage("garden.png");
+    mouseImg1=loadAnimation("mouse1.png");
+    catImg1=loadAnimation("cat1.png");
+    mouseImg2=loadAnimation("mouse2.png","images/mouse3.png");
+    catImg2=loadAnimation("cat2.png","images/cat3.png")
+    mouseImg3=loadAnimation("mouse4.png");
+    catImg3=loadAnimation("cat4.png");
 }
 
 function setup(){
